056b5946c51b6d2300de5d279e3968101f90162a
YuyaKouguchi <134293910+kyuya5655@users.noreply.github.com>
2023-08-05 07:03:24 +0000

    20230805

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @user = current_user
+    @user_introduction = @user.introduction
+    @user_profile_image = @user.profile_image
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params_update)
+       flash[:notice] = "You have updated user successfully."
+       redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params_update
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+end
---
+++ test/controllers/image_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class ImageControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get image_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get image_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get image_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get image_edit_url
+    assert_response :success
+  end
+end
